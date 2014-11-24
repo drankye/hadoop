89643829b4e4c3e9620207ee13efab22290fee8d
chenlin1 <lin1.chen@intel.com>
2014-11-24 11:47:30 +0800

    Raw Java RS EC Coder

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/ec/coder/JavaRSErasureCoder.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/ec/coder/JavaRSErasureCoder.java
@@ -2,7 +2,6 @@ package org.apache.hadoop.hdfs.ec.coder;
 
 import org.apache.hadoop.hdfs.ec.ECChunk;
 import org.apache.hadoop.hdfs.ec.ECSchema;
-import org.apache.hadoop.hdfs.ec.coder.old.impl.help.GaloisField;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -14,97 +13,56 @@ import java.util.List;
  */
 public class JavaRSErasureCoder extends RSErasureCoder {
 
-	private int[] generatingPolynomial;
-	private int PRIMITIVE_ROOT = 2;
-	private int[] primitivePower;
-	private GaloisField GF = GaloisField.getInstance();
-	private int[] paritySymbolLocations;
-	private int[] errSignature;
-	private int[] dataBuff;
-	
 	private RawErasureCoder rawErasureCoder;
 
 	@Override
 	public void initWith(ECSchema schema) {
 		super.initWith(schema);
-		init(schema.getDataBlocks(), schema.getParityBlocks());
-	}
-
-	private void init(int dataSize, int paritySize) {
-		assert (dataSize + paritySize < GF.getFieldSize());
-		 this.errSignature = new int[paritySize];
-		this.paritySymbolLocations = new int[paritySize];
-		 this.dataBuff = new int[paritySize + dataSize];
-		this.rawErasureCoder = new AbstractRawErasureCoder(dataSize, paritySize);
-		for (int i = 0; i < paritySize; i++) {
-			paritySymbolLocations[i] = i;
-		}
-
-		this.primitivePower = new int[dataSize + paritySize];
-		// compute powers of the primitive root
-		for (int i = 0; i < dataSize + paritySize; i++) {
-			primitivePower[i] = GF.power(PRIMITIVE_ROOT, i);
-		}
-		// compute generating polynomial
-		int[] gen = { 1 };
-		int[] poly = new int[2];
-		for (int i = 0; i < paritySize; i++) {
-			poly[0] = primitivePower[i];
-			poly[1] = 1;
-			gen = GF.multiply(gen, poly);
-		}
-		// generating polynomial has all generating roots
-		generatingPolynomial = gen;
+		rawErasureCoder = new JavaRSRawErasureCoder(schema.getDataBlocks(), schema.getParityBlocks());
 	}
 
 	@Override
 	public void encode(ECChunk[] dataChunks, ECChunk outputChunk) {
+		encode(dataChunks, new ECChunk[]{outputChunk});
 	}
 
 	@Override
 	public void encode(ECChunk[] dataChunks, ECChunk[] outputChunks) {
-		final int dataSize = rawErasureCoder.dataSize();
-		final int paritySize = rawErasureCoder.paritySize();
-		assert (dataSize == dataChunks.length);
-		assert (paritySize == outputChunks.length);
-
-		for (int i = 0; i < outputChunks.length; i++) {
-			outputChunks[i].fillZero();
-		}
-
-		byte[][] data = new byte[dataSize + paritySize][];
-
-		for (int i = 0; i < paritySize; i++) {
-			data[i] = outputChunks[i].getChunkBuffer().array();
-		}
-		for (int i = 0; i < dataSize; i++) {
-			data[i + paritySize] = dataChunks[i].getChunkBuffer().array();
-		}
-
-		// Compute the remainder
-		GF.remainder(data, generatingPolynomial);
-
+		ByteBuffer[] dataByteBuffers = changeToByteBufferArray(dataChunks);
+		ByteBuffer[] outputByteBuffers = changeToByteBufferArray(outputChunks);
+		
+		rawErasureCoder.encode(dataByteBuffers, outputByteBuffers);
+		outputChunks = changeToChunkArray(outputByteBuffers);
 	}
 
 	@Override
 	public void decode(ECChunk[] dataChunks, ECChunk[] parityChunks,
 			String annotation, ECChunk outputChunk) {
+		decode(dataChunks, parityChunks, annotation, new ECChunk[]{outputChunk});
 	}
 
 	@Override
 	public void decode(ECChunk[] dataChunks, ECChunk[] parityChunks,
 			String annotation, ECChunk[] outputChunks) {
 		ECChunk[] readChunks = combineArrays(parityChunks, dataChunks);
-		ArrayList<Integer> erasedLocation = getErasedLocation(annotation);
-		decode(readChunks, outputChunks, erasedLocation);
+		ByteBuffer[] readBufs = changeToByteBufferArray(readChunks);
+		ByteBuffer[] outputBufs = changeToByteBufferArray(outputChunks);
+		
+		List<Integer> erasedLocationList = getErasedLocation(annotation);
+		int[] erasedLocationArray = new int[erasedLocationList.size()];
+		for (int i = 0; i < erasedLocationList.size(); i++) {
+			erasedLocationArray[i] = erasedLocationList.get(i);
+		}
+		
+		rawErasureCoder.decode(readBufs, outputBufs, erasedLocationArray);
 	}
 	
 	public int symbolSize() {
 		return rawErasureCoder.symbolSize();
 	}
 
-	private ArrayList<Integer> getErasedLocation(final String annotation) {
-		ArrayList<Integer> erasedLocationArrayList = new ArrayList<Integer>();
+	private List<Integer> getErasedLocation(final String annotation) {
+		List<Integer> erasedLocationArrayList = new ArrayList<Integer>();
 
 		for (int i = 0; i < annotation.length(); i += 2) {
 			char c = annotation.charAt(i);
@@ -121,34 +79,22 @@ public class JavaRSErasureCoder extends RSErasureCoder {
 		return erasedLocationArrayList;
 	}
 
-	private void decode(ECChunk[] readBufs, ECChunk[] writeBufs, List<Integer> erasedLocation) {
-		if (erasedLocation.size() == 0) {
-			return;
-		}
-
-		// cleanup the write buffer
-		for (int i = 0; i < writeBufs.length; i++) {
-			writeBufs[i].fillZero();
-		}
-
-		ByteBuffer[] readBuffersWithByteBuffers = changeByteBufferArray(readBufs);
-		ByteBuffer[] writeBuffersWithByteBuffers = changeByteBufferArray(writeBufs);
-		
-		for (int i = 0; i < erasedLocation.size(); i++) {
-			errSignature[i] = primitivePower[erasedLocation.get(i)];
-			GF.substitute(readBuffersWithByteBuffers, writeBuffersWithByteBuffers[i], primitivePower[i]);
-		}
-		GF.solveVandermondeSystem(errSignature, writeBuffersWithByteBuffers,
-				erasedLocation.size(), readBufs[0].getChunkBuffer().array().length);
-	}
-
-	private ByteBuffer[] changeByteBufferArray(ECChunk[] writeBufs) {
+	private ByteBuffer[] changeToByteBufferArray(ECChunk[] writeBufs) {
 		ByteBuffer[] buffers = new ByteBuffer[writeBufs.length];
 		
 		for (int i = 0; i < writeBufs.length; i++) {
 			buffers[i] = writeBufs[i].getChunkBuffer();
 		}
 		return buffers;
+	}
+	
+	private ECChunk[] changeToChunkArray(ByteBuffer[] writeBufs) {
+		ECChunk[] chunks = new ECChunk[writeBufs.length];
+		
+		for (int i = 0; i < writeBufs.length; i++) {
+			chunks[i] = new ECChunk(writeBufs[i]);
+		}
+		return chunks;
 	}
 
 	private ECChunk[] combineArrays(ECChunk[] array1, ECChunk[] array2) {
--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/ec/coder/JavaRSRawErasureCoder.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/ec/coder/JavaRSRawErasureCoder.java
@@ -15,16 +15,18 @@ public class JavaRSRawErasureCoder extends AbstractRawErasureCoder {
 	private GaloisField GF = GaloisField.getInstance();
 	private int[] paritySymbolLocations;
 	private int[] errSignature;
-	
+	private int[] dataBuff;
+
 	public JavaRSRawErasureCoder(int dataSize, int paritySize) {
 		super(dataSize, paritySize);
 		init();
 	}
-	
+
 	private void init() {
 		assert (dataSize + paritySize < GF.getFieldSize());
-		 this.errSignature = new int[paritySize];
+		this.errSignature = new int[paritySize];
 		this.paritySymbolLocations = new int[paritySize];
+		this.dataBuff = new int[paritySize + dataSize];
 		for (int i = 0; i < paritySize; i++) {
 			paritySymbolLocations[i] = i;
 		}
@@ -45,14 +47,29 @@ public class JavaRSRawErasureCoder extends AbstractRawErasureCoder {
 		// generating polynomial has all generating roots
 		generatingPolynomial = gen;
 	}
-	
+
+	@Override
+	public void encode(int[] message, int[] parity) {
+		assert (message.length == dataSize && parity.length == paritySize);
+		for (int i = 0; i < paritySize; i++) {
+			dataBuff[i] = 0;
+		}
+		for (int i = 0; i < dataSize; i++) {
+			dataBuff[i + paritySize] = message[i];
+		}
+		GF.remainder(dataBuff, generatingPolynomial);
+		for (int i = 0; i < paritySize; i++) {
+			parity[i] = dataBuff[i];
+		}
+	}
+
 	@Override
 	public void encode(ByteBuffer[] inputs, ByteBuffer[] outputs) {
 		assert (dataSize == inputs.length);
 		assert (paritySize == outputs.length);
 
-		for (int i = 0; i < inputs.length; i++) {
-			Arrays.fill(outputs, 0);
+		for (int i = 0; i < outputs.length; i++) {
+			Arrays.fill(outputs[i].array(), (byte) 0);
 		}
 
 		byte[][] data = new byte[dataSize + paritySize][];
@@ -68,20 +85,39 @@ public class JavaRSRawErasureCoder extends AbstractRawErasureCoder {
 	}
 
 	@Override
-	public void decode(ByteBuffer[] readBufs, ByteBuffer[] writeBufs, int[] erasedLocation) {
+	public void decode(int[] data, int[] erasedLocations, int[] erasedValues) {
+		if (erasedLocations.length == 0) {
+			return;
+		}
+		assert (erasedLocations.length == erasedValues.length);
+		for (int i = 0; i < erasedLocations.length; i++) {
+			data[erasedLocations[i]] = 0;
+		}
+		for (int i = 0; i < erasedLocations.length; i++) {
+			errSignature[i] = primitivePower[erasedLocations[i]];
+			erasedValues[i] = GF.substitute(data, primitivePower[i]);
+		}
+		GF.solveVandermondeSystem(errSignature, erasedValues,
+				erasedLocations.length);
+	}
+
+	@Override
+	public void decode(ByteBuffer[] readBufs, ByteBuffer[] writeBufs,
+			int[] erasedLocation) {
 		if (erasedLocation.length == 0) {
 			return;
 		}
 
 		// cleanup the write buffer
 		for (int i = 0; i < writeBufs.length; i++) {
-			Arrays.fill(writeBufs, 0);
+			Arrays.fill(writeBufs[i].array(), (byte) 0);
 		}
 
 		for (int i = 0; i < erasedLocation.length; i++) {
 			errSignature[i] = primitivePower[erasedLocation[i]];
 			GF.substitute(readBufs, writeBufs[i], primitivePower[i]);
 		}
+		
 		GF.solveVandermondeSystem(errSignature, writeBufs,
 				erasedLocation.length, readBufs[0].array().length);
 	}
