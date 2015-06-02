0ef824da6947e29d3c703379979d2231136c7c3d
Drankye <drankye@gmail.com>
2015-06-03 05:15:31 +0800

    wip

---
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/io/erasurecode/rawcoder/util/ErasureCodeUtil.java
@@ -0,0 +1,39 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.hadoop.io.erasurecode.rawcoder.util;
+
+/**
+ * An erasure code util class.
+ */
+public final class ErasureCodeUtil {
+
+  private ErasureCodeUtil() { }
+
+  public static void initTables(int k, int rows, byte[] a, byte[] gftbls) {
+    int i, j;
+
+    for (i = 0; i < rows; i++) {
+      for (j = 0; j < k; j++) {
+        GaloisFieldUtil.gfVectMulInit(*a++, gftbls);
+        gftbls += 32;
+      }
+    }
+  }
+
+
+}
