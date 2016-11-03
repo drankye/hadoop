0ec0c9af010594cda89fd17d455368841022e2f1
timmyyao <shunyang.yao@intel.com>
2016-11-03 09:58:10 +0800

    process rename and delete in DecisionMaker

--- hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/SSMServer.java
+++ hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/SSMServer.java
@@ -45,7 +45,7 @@ public class SSMServer {
 
   public static void main(String[] args) throws Exception {
     DFSClient dfsClient = new DFSClient(DFSUtilClient.getNNAddress(conf), conf);
-    DecisionMaker decisionMaker = new DecisionMaker(50);
+    DecisionMaker decisionMaker = new DecisionMaker(50, "ONE_SSD");
     Timer timer = new Timer();
     timer.schedule(new DecisionMakerTask(dfsClient, decisionMaker), 2*1000L, 5*60*1000L);
   }
