9534744540f31c8fc1ebc882a0261109487d3723
= <liwenfeisix@163.com>
2016-12-12 11:11:06 +0800

    add the MAX_Map_Num test

--- hadoop-ssm-project/src/test/java/org/apache/hadoop/ssm/RuleContainerTest.java
+++ hadoop-ssm-project/src/test/java/org/apache/hadoop/ssm/RuleContainerTest.java
@@ -163,4 +163,97 @@ public class RuleContainerTest {
         assertEquals(false, ruleContainer.getWindowMap().getWindowMaps().contains(fileAccessMapB));
         assertEquals(false, ruleContainer.getWindowMap().getFileAccessMapInWindow().containsKey(fileNameB));
     }
+
+
+    @Test
+    public void testContainerMax_Map_Num() {
+        SSMRule ruleObject = SSMRuleParser.parseAll("file.path matches('/A/[a-z]*') : accessCount(40 min) >= 1 | cache").get();
+        //second
+        long updateDuration = 1 * 60;
+        DFSClient dfsClient = null;
+        RuleContainer ruleContainer = new RuleContainer(ruleObject, updateDuration, dfsClient);
+        String fileNameA = "/A/a";
+        String fileNameB = "/A/b";
+        String fileNameC = "/A/c";
+        String fileNameD = "/A/d";
+        FileAccess fileAccessA = new FileAccess(fileNameA, 10);
+        FileAccess fileAccessB = new FileAccess(fileNameB, 11);
+        FileAccess fileAccessC = new FileAccess(fileNameC, 12);
+        FileAccess fileAccessD = new FileAccess(fileNameD, 13);
+        FileAccessMap fileAccessMapA = new FileAccessMap();
+        fileAccessMapA.put(fileNameA, fileAccessA);
+        FileAccessMap fileAccessMapB = new FileAccessMap();
+        fileAccessMapB.put(fileNameB, fileAccessB);
+        FileAccessMap fileAccessMapC = new FileAccessMap();
+        fileAccessMapC.put(fileNameC, fileAccessC);
+        FileAccessMap fileAccessMapD = new FileAccessMap();
+        fileAccessMapD.put(fileNameD, fileAccessD);
+        ArrayList<Map<String, Integer>> listMap = new ArrayList<>();
+        Map<String, Integer> mapA = new HashMap<String, Integer>();
+        mapA.put(fileNameA, 10);
+        listMap.add(mapA);
+        Map<String, Integer> mapB = new HashMap<String, Integer>();
+        mapB.put(fileNameB, 11);
+        listMap.add(mapB);
+        Map<String, Integer> mapC = new HashMap<String, Integer>();
+        mapC.put(fileNameC, 12);
+        listMap.add(mapC);
+        Map<String, Integer> mapD = new HashMap<String, Integer>();
+        mapD.put(fileNameD, 13);
+        listMap.add(mapD);
+        NNEvent nnEventA = new NNEvent(0);
+        NNEvent nnEventB = new NNEvent(0);
+        NNEvent nnEventC = new NNEvent(0);
+        NNEvent nnEventD = new NNEvent(0);
+        List<List<NNEvent>> listList = new ArrayList<List<NNEvent>>();
+        List<NNEvent> nnEventListA = new ArrayList<NNEvent>();
+        nnEventListA.add(nnEventA);
+        listList.add(nnEventListA);
+        List<NNEvent> nnEventListB = new ArrayList<NNEvent>();
+        nnEventListB.add(nnEventB);
+        listList.add(nnEventListB);
+        List<NNEvent> nnEventListC = new ArrayList<NNEvent>();
+        nnEventListC.add(nnEventC);
+        listList.add(nnEventListC);
+        List<NNEvent> nnEventListD = new ArrayList<NNEvent>();
+        nnEventListD.add(nnEventD);
+        listList.add(nnEventListD);
+        FilesAccessInfo filesAccessInfoA = new FilesAccessInfo();
+        FilesAccessInfo filesAccessInfoB = new FilesAccessInfo();
+        FilesAccessInfo filesAccessInfoC = new FilesAccessInfo();
+        FilesAccessInfo filesAccessInfoD = new FilesAccessInfo();
+        filesAccessInfoA.setAccessCounter(listMap.get(0));
+        filesAccessInfoA.setNnEvents(listList.get(0));
+        filesAccessInfoB.setAccessCounter(listMap.get(1));
+        filesAccessInfoB.setNnEvents(listList.get(1));
+        filesAccessInfoC.setAccessCounter(listMap.get(2));
+        filesAccessInfoC.setNnEvents(listList.get(2));
+        filesAccessInfoD.setAccessCounter(listMap.get(3));
+        filesAccessInfoD.setNnEvents(listList.get(3));
+
+        HashMap<String, Action> hm = new HashMap<String, Action>();
+        //put D
+        ruleContainer.update(filesAccessInfoD);
+        hm = ruleContainer.actionEvaluator(fileAccessMapD);
+        //a step is 2minute
+        assertEquals(false,ruleContainer.getWindowMap().getWindowMaps().contains(fileAccessMapD));
+        //DA
+        ruleContainer.update(filesAccessInfoA);
+        hm = ruleContainer.actionEvaluator(fileAccessMapA);
+        assertEquals(true,ruleContainer.getWindowMap().getWindowMaps().getFirst().containsKey(fileNameD));
+        assertEquals(true,ruleContainer.getWindowMap().getWindowMaps().getFirst().containsKey(fileNameA));
+        //(D)ABBBBBBBBBBBBBBBBBBC
+        for(int i=0; i<18;i++ ){
+            ruleContainer.update(filesAccessInfoB);
+            hm = ruleContainer.actionEvaluator(fileAccessMapB);
+        }
+        ruleContainer.update(filesAccessInfoC);
+        hm = ruleContainer.actionEvaluator(fileAccessMapC);
+        assertEquals(false,ruleContainer.getWindowMap().getWindowMaps().contains(fileAccessMapD));
+        //contain 20map
+        assertEquals(0,ruleContainer.getWindowMap().getWindowMaps().getFirst().get(fileNameA).compareTo(fileAccessA));
+        assertEquals(0,ruleContainer.getWindowMap().getWindowMaps().getLast().get(fileNameC).compareTo(fileAccessC));
+    }
+
+
 }
